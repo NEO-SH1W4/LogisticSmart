f25c8cd9199210140d3dbff724e6ca796afd5e2f
NEO-SH1W4 <joao.oliveirax@icloud.com>
2025-03-22 14:05:52 -0300

    🧹 Renomeações e fusão de scripts para padronização do projeto

--- Iniciar_Logistica.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\JX\Desktop\Logistc_BETA"
-start /min "" streamlit run app_logistica_web.py
-exit
--- start_LogisticSmart.bat
+++ start_LogisticSmart.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Iniciando o aplicativo LogisticSmart...
-streamlit run LogisticSmart_v1.py --server.enableXsrfProtection false --server.enableCORS false --server.address 0.0.0.0 --server.port 8501
+title 🚀 Iniciando LogisticSmart - Web App
 echo.
-echo Pressione qualquer tecla para fechar...
-pause > nul
+echo Abrindo o aplicativo Logístico Inteligente...
+streamlit run LogisticSmart_v1.py --server.enableXsrfProtection false --server.enableCORS false --server.address 0.0.0.0 --server.port 8501
+pause
